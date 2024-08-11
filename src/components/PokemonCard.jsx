9c62d5447da6db4c16208c521e96fe0ae2a08f9c
ayushhkkkk3585 <ayushkhalate04@gmail.com>
2024-08-11 22:14:34 +0530

    css changes i.e pokemon type changes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,12 +3,8 @@ import React from "react";
 const PokemonCard = ({ pokemonData }) => {
   return (
     <>
-   
-    
       <div className="flex flex-col w-96 mx-auto rounded overflow-hidden shadow-lg">
         <div className="flex justify-center items-center h-40">
-          {" "}
-          {/* Centering the image */}
           <img
             className="w-28 mt-2 h-auto"
             src={pokemonData.sprites.other.dream_world.front_default}
@@ -19,22 +15,21 @@ const PokemonCard = ({ pokemonData }) => {
           <div className="font-bold text-xl text-center mb-3">
             {pokemonData.name}
           </div>
-          <span className="text-lg text-white font-semibold text-center bg-green-500 px-4 py-2 rounded-xl ">
-            {pokemonData.types.map((currType) => currType.type.name).join(", ")}
-          </span>
+          <div className="flex justify-center"> {/* Added this div for centering */}
+            <span className="text-lg text-white font-semibold text-center bg-green-500 px-4 py-2 rounded-xl">
+              {pokemonData.types.map((currType) => currType.type.name).join(", ")}
+            </span>
+          </div>
         </div>
         <div className="flex justify-center items-center gap-2">
           <span className="text-base font-medium">
-            {" "}
-            Height:{pokemonData.height}{" "}
+            Height:{pokemonData.height}
           </span>
           <span className="text-base font-medium">
-            {" "}
-            Weight:{pokemonData.weight}{" "}
+            Weight:{pokemonData.weight}
           </span>
           <span className="text-base font-medium">
-            {" "}
-            Speed:{pokemonData.stats[5].base_stat}{" "}
+            Speed:{pokemonData.stats[5].base_stat}
           </span>
         </div>
         <div className="px-6 pt-4 pb-2 mb-3 flex justify-center">
